310d8693710b1ed3949ed059468622d65ffd9586
zxl <1024659300@qq.com>
2019-04-09 15:22:00 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    /**显示个人资料
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(User $user){
         return view('users.show', compact('user'));
+    }
+
+    /**编辑个人资料
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(User $user){
+        return view('users.edit', compact('user'));
+    }
+    public function update(UserRequest $request,User $user){
+    $user->update($request->all());
+    return redirect()->route("users.show",$user->id)->with("success","个人资料更新成功！");
     }
 }
