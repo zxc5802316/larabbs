79527f5510b0cf4a373af97d848ae7d3b0d5a8fb
zxl <1024659300@qq.com>
2019-04-10 13:58:52 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,20 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /** 一对一 关联 关联 category 一个话题属于一个分类；
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    /**一对一 关联 关联 User 一个话题拥有一个作者。
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
