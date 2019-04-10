9acfeebe8b35c268da1df6270c1d3614a2eb7bd6
zxl <1024659300@qq.com>
2019-04-10 15:35:01 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,8 +8,9 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-  public function show(Category $category){
-      $topics = Topic::where('category_id',$category->id)->paginate(20);
+  public function show(Category $category,Request $request,Topic $topic){
+//      dump($request->order);
+      $topics = $topic->where('category_id',$category->id)->withOrder($request->order)->paginate(20);
       return view("topics.index",compact("topics","category"));
   }
 }
