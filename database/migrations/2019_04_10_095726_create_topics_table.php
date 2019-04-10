1014ce6bbfb625e52d024b3c5124374f97e28027
zxl <1024659300@qq.com>
2019-04-10 10:04:17 +0800

    生成话题骨架

---
+++ database/migrations/2019_04_10_095726_create_topics_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTopicsTable extends Migration 
+{
+	public function up()
+	{
+		Schema::create('topics', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->index()->comment("帖子标题");
+            $table->text('body')->comment('帖子内容');
+            $table->integer('user_id')->unsigned()->index()->comment("用户 ID");
+            $table->integer('category_id')->unsigned()->index()->comment('分类 ID');
+            $table->integer('reply_count')->unsigned()->default(0)->comment("回复数量");
+            $table->integer('view_count')->unsigned()->default(0)->comment("查看总数");
+            $table->integer('last_reply_user_id')->unsigned()->default(0)->comment("最后回复的用户 ID");
+            $table->integer('order')->unsigned()->default(0)->comment('可用来做排序使用');
+            $table->text('excerpt')->nullable()->comment("文章摘要，SEO 优化时使用");
+            $table->string('slug')->nullable()->comment("SEO 友好的 URI");
+            $table->timestamps();
+        });
+	}
+
+	public function down()
+	{
+		Schema::drop('topics');
+	}
+}
