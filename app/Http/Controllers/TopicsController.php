289d44c37ae0409a7eefb3ef85498dafc76ad3ea
zxl <1024659300@qq.com>
2019-04-11 11:25:54 +0800

    翻译 URL 标示

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -22,8 +22,11 @@ class TopicsController extends Controller
 		return view('topics.index', compact('topics'));
 	}
 
-    public function show(Topic $topic)
+    public function show(Request $request,Topic $topic)
     {
+        if(! empty($topic->slug) && $topic->slug != $request->slug){
+            return redirect($topic->link());
+        }
         return view('topics.show', compact('topic'));
     }
 
@@ -38,7 +41,7 @@ class TopicsController extends Controller
 		$topic->fill($request->all());
 		$topic->user_id = \Auth::id();
 		$topic->save();
-		return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功！');
+		return redirect()->to($topic->link())->with('success', '帖子创建成功！');
 	}
 
 	public function edit(Topic $topic)
