3b6d64bc330d45d900c0e99132a6046ee1450713
zxl <1024659300@qq.com>
2019-04-11 15:53:08 +0800

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable implements MustVerifyEmailContract
 {
     use MVET;
-    use Notifiable ;
+    use Notifiable;
+
     public function toNotify($instance)
     {
         // 如果要通知的人是当前用户，就不必通知了！
@@ -19,16 +20,21 @@ class User extends Authenticatable implements MustVerifyEmailContract
         }
 
         // 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
-            $this->increment('notification_count');
-             $this->notify($instance);
+        $this->increment('notification_count');
+        $this->notify($instance);
     }
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','introduction','avatar'
+        'name',
+        'email',
+        'password',
+        'introduction',
+        'avatar',
     ];
 
     /**
@@ -37,7 +43,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
@@ -52,22 +59,35 @@ class User extends Authenticatable implements MustVerifyEmailContract
     /**模型关联 一个用户 有多个话题  一对多
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    public function topics(){
+    public function topics()
+    {
         return $this->hasMany(Topic::class);
     }
 
     /**一个用户可以拥有多条评论
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    public function replies(){
+    public function replies()
+    {
         return $this->hasMany(Reply::class);
     }
 
-    public function isAuthOf($model){
+    public function isAuthOf($model)
+    {
         return $this->id == $model->user_id;
     }
+
     public function scopeRecent($query)
     {
         return $query->orderBy('id', 'desc');
     }
+
+    // 清除未读消息标示
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
+
 }
