17106235e4cac6af68a847e5cd5df894230b1262
zxl <1024659300@qq.com>
2019-04-11 14:47:31 +0800

    用户可以发表评论

--- routes/web.php
+++ routes/web.php
@@ -41,4 +41,4 @@ Route::get('topics/{topic}/{slug?}','TopicsController@show')->name("topics.show"
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 //上传图片
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('replies', 'RepliesController', ['only' => [ 'store','destroy']]);
