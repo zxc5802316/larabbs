03264bb00eef9c08bea5b518bfc53b33c805025c
zxl <1024659300@qq.com>
2019-04-12 10:25:13 +0800

    站点设置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{--CSRF Token--}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
+    <meta name="description" content="@yield('description', setting('seo_description', '筱龙blog 社区。'))" />
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', '筱龙blog,社区,论坛,开发者论坛'))" />
 
-    <title>@yield('title',"LaraBBS") - Laravel 进阶教程</title>
+    <title>@yield('title', '筱龙blog') - {{ setting('site_name', '筱龙blog') }}</title>
     {{--style--}}
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     @yield('styles')
