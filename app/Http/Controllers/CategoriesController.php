8f08602b31cba01766a54f716dea1da0c0334067
zxl <1024659300@qq.com>
2019-04-12 11:27:42 +0800

    侧边栏链接

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,19 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-  public function show(Category $category,Request $request,Topic $topic,User $user){
+  public function show(Category $category,Request $request,Topic $topic,User $user,Link $link){
 //      dump($request->order);
       $topics = $topic->where('category_id',$category->id)->withOrder($request->order)->paginate(20);
       // 活跃用户列表
       $active_users = $user->getActiveUsers();
-
+        $links = $link->getAllCached();
       // 传参变量话题和分类到模板中
-      return view('topics.index', compact('topics', 'category', 'active_users'));
+      return view('topics.index', compact('topics', 'category', 'active_users','links'));
   }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+
+
 use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 
@@ -32,7 +34,8 @@ class AppServiceProvider extends ServiceProvider
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
-
+        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
+        // Carbon 中文化配置
         Carbon::setLocale("zh");
     }
 }
